05dde5e61f95799abc8e7cbf714f086ca4576f81
DELL <DELL@DESKTOP-HHGMRH8>
2022-06-14 21:21:43 +0530

    commit

---
+++ Practice/src/Package1/student.java
@@ -0,0 +1,21 @@
+package Package1;
+public class student{
+	int roll,age;
+	void display1() {
+		System.out.println("Roll no of the student");
+		}
+	void display2() {
+		System.out.println("Age of the student");
+	}
+	public static void main(String[]args) {
+		student xyz=new student();
+		xyz.display1();
+		xyz.roll=2;
+		System.out.println(xyz.roll);
+		xyz.display2(); 
+		xyz.age=30;
+		System.out.println(xyz.age);
+	}
+	
+}
+	
